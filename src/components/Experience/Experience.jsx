a066e98ba89a9d62caa191924ccec1bcef6a09fb
RMerkulov <fotoromario@gmail.com>
2023-02-08 11:40:31 +0200

    upd

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -34,15 +34,15 @@ export const Experience = () => {
             <div className={s.experienceBox}>
               <img className={s.aboutImg} src={Altertech} alt="portfolio" />
               <p className={s.aboutText}>
-                07.08.2022 - 01.11.2022 <br />
-                Trainee in Altertech
+                From 01.11.2022 <br />
+                Front-end developer in Altertech
               </p>
             </div>
             <div className={s.experienceBox}>
               <img className={s.aboutImg} src={Altertech} alt="portfolio" />
               <p className={s.aboutText}>
-                From 01.11.2022 <br />
-                Front-end developer in Altertech
+                07.08.2022 - 01.11.2022 <br />
+                Trainee in Altertech
               </p>
             </div>
           </div>
